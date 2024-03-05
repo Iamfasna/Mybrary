2d7b5059d73e901d1e1ee25f16fa91d34248f686
Fasna F F <fasnaff2@gmail.com>
2024-03-05 15:02:06 +0530

    initial setup

---
+++ server.js
@@ -0,0 +1,32 @@
+
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+
+const indexRouter = require('./routes/index')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/Mybrary', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+
+app.use('/', indexRouter)
+
+app.listen(process.env.PORT || 3000)
